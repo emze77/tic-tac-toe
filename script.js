4e4008a9f154080bbf82d4c5dfc420995609ffc9
emze <kontakt@marcus-sperber.de>
2025-05-29 14:52:37 +0200

    feat.: render board in console

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-function gameboard () {
-    const newGameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let actualGameboard = newGameboard;
-    
-    const placeX = (field) => {
-        actualGameboard.splice((field - 1), 1, "X")
-        console.log(actualGameboard)
-    };
+function gameboard() {
+  let gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    const placeO = (field) => {
-        actualGameboard.splice((field - 1), 1, "O")
-        console.log(actualGameboard)
-    }
+  const placeX = (field) => {
+    gameboard.splice(field - 1, 1, "X");
+  };
 
-    const clearGame = () => {
-        actualGameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        console.log(actualGameboard);
-    }
+  const placeO = (field) => {
+    gameboard.splice(field - 1, 1, "O");
+  };
 
-    return { gameboard , placeX , placeO , clearGame}
+  const clearGame = () => {
+    gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  };
+
+  return { gameboard, placeX, placeO, clearGame };
 }
+
+function renderConsole (gameboard) {
+ console.log(
+    " " + gameboard[0], gameboard[1], gameboard[2], "\n",
+    gameboard[3], gameboard[4], gameboard[5], "\n",
+    gameboard[6], gameboard[7], gameboard[8], "\n",
+ )
+}
+
 
 
 const newGame = gameboard();
 
 newGame.placeX(5);
-newGame.placeO(4)
-newGame.placeX(3)
-newGame.clearGame()
+newGame.placeO(4);
+newGame.placeX(3);
+renderConsole(newGame.gameboard);
+newGame.clearGame();
