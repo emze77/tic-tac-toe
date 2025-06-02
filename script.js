7f51eafb1ee61ed31aa16f39c067fb66c0401452
emze <kontakt@marcus-sperber.de>
2025-06-02 20:45:24 +0200

    feat.: new function to verify legal placement

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 
 const gameboard = (function () {
   let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const placePlayerOne = (field) => board.splice(field - 1, 1, game.playerOne.checkSymbol());
-  const placePlayerTwo = (field) => board.splice(field - 1, 1, game.playerTwo.checkSymbol());
+  const placePlayerOne = (field) =>
+    board.splice(field - 1, 1, game.playerOne.checkSymbol());
+  const placePlayerTwo = (field) =>
+    board.splice(field - 1, 1, game.playerTwo.checkSymbol());
   const clearBoard = () => (board = [1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const logGameboard = () => renderConsole.board(board);
-  return { board, placePlayerOne, placePlayerTwo, logGameboard, clearBoard };
+  return {
+    board,
+    placePlayerOne,
+    placePlayerTwo,
+    logGameboard,
+    clearBoard,
+  };
 })();
 
 // ___PLAYER___
@@ -42,8 +50,10 @@ const game = (function () {
   };
 
   const playTurn = () => {
-
-  }
+    renderConsole.nextTurn();
+    let nextTurn = prompt("What`s your turn? (1 - 9)");
+    verifyTurn(nextTurn);
+  };
 
   const resetGame = () => {
     gameboard.clearBoard();
@@ -51,17 +61,30 @@ const game = (function () {
     playerTwo.score = 0;
   };
 
-  const randomStartPlayer =  () => {
+  const randomStartPlayer = () => {
     if (Math.random() < 0.5) {
       turnPlayerOne = true;
-      console.log("Player One Begins!")
+      console.log("Player One Begins!");
     } else {
       turnPlayerOne = false;
-      console.log("Player Two Begins!")
+      console.log("Player Two Begins!");
     }
-  }
+  };
 
-
+  const verifyTurn = (field) => {
+    if (
+      gameboard.board[field - 1] === game.playerOne.checkSymbol() ||
+      gameboard.board[field - 1] === game.playerTwo.checkSymbol()
+    ) {
+      renderConsole.invalidTurn("Field is already occupied!");
+    } else if (field < 1 || field > 9 || field % 1 !== 0) {
+      renderConsole.invalidTurn("Not a valid number!");
+    } else if (isNaN(field)) {
+      renderConsole.invalidTurn("Not a number at all!");
+    } else {
+      return true;
+    }
+  };
 
   return {
     gameCounter,
@@ -71,19 +94,13 @@ const game = (function () {
     turnPlayerOne,
     newGame,
     resetGame,
-    randomStartPlayer,
-    playRound,
-    playTurn,
   };
 })();
 
-
-const renderConsole = (function() {
-
+const renderConsole = (function () {
   const board = (board) => {
     console.log(
-      "\n" + 
-      " " + board[0],
+      "\n" + " " + board[0],
       board[1],
       board[2],
       "\n",
@@ -96,41 +113,33 @@ const renderConsole = (function() {
       board[8],
       "\n"
     );
-  }
+  };
 
   const score = () => {
     console.log(
-      `Score Player One: ${game.playerOne.checkScore()}\n` + 
-      `Score Player Two: ${game.playerTwo.checkScore()}\n`
+      `Score Player One: ${game.playerOne.checkScore()}\n` +
+        `Score Player Two: ${game.playerTwo.checkScore()}\n`
     );
-  }
+  };
 
-  return { board , score }
+  const nextTurn = () => {
+    let nextPlayer = "";
+    if (game.turnPlayerOne) {
+      nextPlayer = "Player One";
+    } else {
+      nextPlayer = "Player Two";
+    }
+    console.log(`Next turn: ${nextPlayer}!`);
+  };
+
+  const invalidTurn = (message) => {
+    console.log(message);
+  };
+
+  return { board, score, nextTurn, invalidTurn };
 })();
 
-// function renderConsole(board) {
-//   console.log(
-//     "\n" + 
-//     " " + board[0],
-//     board[1],
-//     board[2],
-//     "\n",
-//     board[3],
-//     board[4],
-//     board[5],
-//     "\n",
-//     board[6],
-//     board[7],
-//     board[8],
-//     "\n"
-//   );
-//   console.log(
-//     `Score Player One: ${game.playerOne.checkScore()}\n` + 
-//     `Score Player Two: ${game.playerTwo.checkScore()}\n`
-//   );
-// }
-
-game.newGame();
 gameboard.placePlayerOne(4);
 gameboard.placePlayerTwo(6);
+game.newGame();
 gameboard.logGameboard();
